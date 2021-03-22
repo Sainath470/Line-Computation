849d2776f5a638b9484cbc02417cc42a21b06888
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-03-23 00:18:01 +0530

    Added:Length of the line using co-ordinates

--- Geometry.java
+++ Geometry.java
@@ -1,1 +1,20 @@
-"Welcome to Line Comparision Computation"
+import java.util.Scanner;
+public class Geometry {
+	 public static void main(String args[])
+	 {
+		 	double LengthOfLine=0;
+		 	Scanner sc=new Scanner(System.in);
+		 	System.out.println("Enter the x1-coordinate");
+		 	int x1=sc.nextInt();
+		 	System.out.println("Enter the y1-coordinate");
+		 	int y1=sc.nextInt();
+		 	System.out.println("Enter the x2-coordinate");
+		 	int x2=sc.nextInt();
+		 	System.out.println("Enter the y2-coordinate");
+		 	int y2=sc.nextInt();
+		 	System.out.println("("+x1+","+y1+")");
+		 	System.out.println("("+x2+","+y2+")");
+		 	LengthOfLine=(int)Math.sqrt(Math.pow((x2-x1),2)+Math.pow((y2-y1),2));
+		 	System.out.println("Length of the line is="+LengthOfLine);
+	   }
+}
